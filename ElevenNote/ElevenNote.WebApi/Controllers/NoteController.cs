2f84d44c89478fe383456bd1e1d16cd8ad6c1aae
Cyrus Spencer <cyrus.spencer119@gmail.com>
2022-03-31 17:41:08 -0400

    nearly there

--- ElevenNote/ElevenNote.WebApi/Controllers/NoteController.cs
+++ ElevenNote/ElevenNote.WebApi/Controllers/NoteController.cs
@@ -1,13 +1,43 @@
-﻿using System;
+﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.ModelBinding;
 
 namespace ElevenNote.WebApi.Controllers
 {
+    [Authorize]
     public class NoteController : ApiController
     {
+        private NoteService CreateNoteService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var noteService = new NoteService(userId);
+            return noteService;
+        }
+    public IHttpActionResult Get(int id)
+    {
+        NoteService noteService = CreateNoteService();
+        var note = noteService.GetNotes();
+        return Ok(note);
+    }
+    public IHttpActionResult Post(NoteCreate note)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        var service = CreateNoteService();
+
+        if (!service.CreateNote(note))
+            return InternalServerError();
+
+        return Ok();
+    }
+
     }
 }
