685d79659414ab3f406abdefe76ea0a86dd8cc6f
Cyrus Spencer <cyrus.spencer119@gmail.com>
2022-03-31 17:45:07 -0400

    should be finished

--- ElevenNote/ElevenNote.WebApi/Controllers/NoteController.cs
+++ ElevenNote/ElevenNote.WebApi/Controllers/NoteController.cs
@@ -39,5 +39,28 @@ namespace ElevenNote.WebApi.Controllers
         return Ok();
     }
 
+        public IHttpActionResult Put(NoteEdit note)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateNoteService();
+
+            if (!service.UpdateNote(note))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateNoteService();
+
+            if (!service.DeleteNote(id))
+                return InternalServerError();
+
+            return Ok();
+        }
+
     }
 }
